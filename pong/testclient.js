878ffba694fb90bac1a16b84df0a62ec0b519d47
gheorghe <chickenslayeggs@gmail.com>
2012-11-27 19:43:30 +0200

    moar nodejs modules

---
+++ pong/testclient.js
@@ -0,0 +1,13 @@
+var io = require('socket.io-client');
+socket = io.connect('http://localhost:1337')
+socket.on('data', function(data) {
+    console.log(data);
+});
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function(data) {
+    var eventType = data.split(" ")[0];
+    socket.emit(eventType, data);
+});
